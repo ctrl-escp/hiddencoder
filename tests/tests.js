310b50543cec4c101ad778dcdeff9bce676de77c
Ben Baryo <ben.baryo@gmail.com>
2021-04-13 18:14:18 +0300

    Improved tests' descriptions

--- tests/tests.js
+++ tests/tests.js
@@ -16,24 +16,26 @@ function test_encode_decode(originalString, coverString) {
 }
 
 const stringsToTest = [
-    'Hello there', 
-    '123456789.0',
-    '123 number and characters 890',
-    '`start and end with backtick`',
-    "'start and end with single quote'",
-    '"start and end with double quote"',
-    '',
-    ' ',
-    '\n\r\t'
+    ['Hello there', 'a regular string'], 
+    ['12345', 'a regular number'],  // That's amazing! I've got the same combination on my luggage!
+    ['1234.567890', 'a number with a decimal point'],
+    ['12.', 'a number that ends with a decimal point'],
+    ['133t', 'mixed digits and characters'],
+    ['`backtick`', 'a string that starts and ends with a backtick'],
+    ["'single quote'", 'a string that starts and ends with a single quote'],
+    ['"double quotes"', 'a string that starts and ends with double quotes'],
+    ['', 'an empty string'],
+    ['     ', 'whitespaces'],
+    ['\n\r\t\b', 'special chars'],
 ];
 
 console.log('Starting Hiddencoder tests...');
 console.time('All tests completed in');
-for (const hideThisString of stringsToTest) {
-    process.stdout.write(`- Testing ${hideThisString}...'\n`);
-    for (const coverString of stringsToTest) {
+for (const [hideThisString, testDescription] of stringsToTest) {
+    process.stdout.write(`- Testing ${testDescription}...'\n`);
+    for (const [coverString, coverDescription] of stringsToTest) {
         console.time('PASS');
-        process.stdout.write(`\twith ${coverString} ...`);
+        process.stdout.write(`\twith ${coverDescription}... `);
         test_encode_decode(hideThisString, coverString);
         console.timeEnd('PASS');
     } 
